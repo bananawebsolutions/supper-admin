40a59aaa54bf0aed09ed3dffd141c4e7988c85ea
Fernando Rojas <fer.rojas95@gmail.com>
2025-01-06 11:05:01 -0600

    added localization for clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import ClientLayout from "./client-layout";
+import { esMX } from "@clerk/localizations";
 
 export const metadata: Metadata = {
     title: "Supper Admin",
@@ -21,7 +22,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <ClerkProvider>
+        <ClerkProvider localization={esMX}>
             <html lang="es" suppressHydrationWarning>
                 <body
                     className={cn(
