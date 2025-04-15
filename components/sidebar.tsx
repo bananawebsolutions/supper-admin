05a163382fbe35570d4e75d659f9adc1bf8514b5
Fernando Rojas <fer.rojas95@gmail.com>
2025-04-14 19:18:41 -0600

    remove badge import from sidebar component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -10,7 +10,7 @@ import {
     Users,
 } from "lucide-react";
 
-import { Badge } from "@/components/ui/badge";
+// import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 // import {
 //     DropdownMenu,
